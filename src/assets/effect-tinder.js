8d1b9fe5e32df94d6008b6cc2cd823c1eac12f8e
vinaykanna <vinaykanna367@gmail.com>
2024-07-06 18:28:11 +0530

    refactor

--- src/assets/effect-tinder.js
+++ src/assets/effect-tinder.js
@@ -1,10 +1,5 @@
 function EffectTinder({ swiper, on }) {
-  let isTouching,
-    isTinderEffect,
-    previousActiveIndex,
-    isAnimating,
-    isSlideChangePending,
-    currentTouchOffsetX;
+  let isTouching, isAnimating, previousActiveIndex;
 
   swiper.tinder = {
     no() {
@@ -67,19 +62,25 @@ function EffectTinder({ swiper, on }) {
     Object.assign(swiper.originalParams, tinderEffectParams);
   });
 
-  on("touchStart", () => {
+  on("touchStart", (_, touch) => {
     if (swiper.params.effect !== "tinder") return;
 
-    isTinderEffect = true;
     isAnimating = true;
-    isSlideChangePending = true;
+    isTouching = true;
 
-    setTransformOriginForSlides("center bottom", "bottom");
+    const closestSlide = touch.target.closest(".swiper-slide, swiper-slide");
+
+    if (
+      closestSlide &&
+      closestSlide.classList.contains("swiper-slide-active")
+    ) {
+      previousActiveIndex = swiper.activeIndex;
+      setTransformOriginForSlides("center bottom", "bottom");
+    }
   });
 
   on("touchEnd", () => {
     if (swiper.params.effect === "tinder") {
-      isTouching = false;
       isAnimating = false;
 
       requestAnimationFrame(() => {
@@ -96,73 +97,41 @@ function EffectTinder({ swiper, on }) {
     }
   });
 
-  on("slideChange", () => {
-    if (!isAnimating) {
-      isSlideChangePending = false;
-      swiper.emit("tinderSwipe", currentTouchOffsetX < 0 ? "left" : "right");
-    }
-  });
-
-  on("transitionStart", () => {
-    if (isSlideChangePending && swiper.activeIndex !== previousActiveIndex) {
-      isSlideChangePending = false;
-      swiper.emit("tinderSwipe", currentTouchOffsetX < 0 ? "left" : "right");
-    }
-  });
-
-  on("setTranslate", (swiperInstance, translate) => {
-    // Check if the effect is "tinder"
+  on("setTranslate", (_, translate) => {
     if (swiper.params.effect !== "tinder") return;
-    if (isTouching) return;
 
-    // Check if the swiper is animating and previousActiveIndex is defined
     if (
       isAnimating &&
       previousActiveIndex !== undefined &&
       swiper.snapGrid[previousActiveIndex + 1] !== undefined
     ) {
-      // Calculate the maximum allowable translate value
       const startTranslate = Math.abs(swiper.snapGrid[previousActiveIndex]);
       const maxTranslate = Math.abs(startTranslate + swiper.size) - 8;
 
-      // If the translate value exceeds the maximum, set it to the maximum
       if (Math.abs(translate) > maxTranslate) {
         swiper.setTranslate(-maxTranslate);
         return;
       }
     }
 
-    // Calculate the current touch offset in the X direction
     const currentXOffset = swiper.touches.currentX - swiper.touches.startX;
-    currentTouchOffsetX = currentXOffset;
 
-    // Get the slides and check if the active index is the last slide
     const { slides } = swiper;
 
-    // Iterate through each slide to set its transform and opacity based on progress
     slides.forEach((slide, index) => {
       const progress = slide.progress;
       const clampedProgress = Math.min(Math.max(progress, -2), 2);
 
       let translateX = -slide.swiperSlideOffset;
-      let translateY = 0;
-      let translateZ = 100 * clampedProgress;
+      let translateY = (index + 1) * 10;
+      let translateZ = (index + 1) * 10 * clampedProgress;
       let rotateZ = 0;
 
-      // Set transform values based on progress and touch offset
-      if (clampedProgress > 0 || (clampedProgress === 0 && isTinderEffect)) {
+      if (clampedProgress > 0 || (clampedProgress === 0 && isTouching)) {
         translateZ = 0;
         rotateZ = 45 * clampedProgress * (currentXOffset < 0 ? -1 : 1);
         translateX +=
           swiper.size * (currentXOffset < 0 ? -1 : 1) * clampedProgress;
-
-        // if (slide.translateY !== undefined) {
-        //   translateY = slide.translateY;
-        // }
-
-        // if (index !== 0) {
-        // translateY = index * -10;
-        // }
       }
 
       console.log({
@@ -171,22 +140,19 @@ function EffectTinder({ swiper, on }) {
         translateZ,
       });
 
-      // Generate transform string
       const transformString = `
         translate3d(${translateX}px, ${translateY}px, ${translateZ}px)
         rotateZ(${rotateZ}deg)
       `;
 
-      // Cache transform for the current slide
       if (clampedProgress >= 1 && !slide.tinderTransform) {
         slide.tinderTransform = transformString;
         slide.tinderTransformSlideIndex = index;
       }
 
-      // Apply transform and opacity to the slide
       if (
         (slide.tinderTransform && slide.tinderTransformSlideIndex !== index) ||
-        !isTinderEffect
+        !isTouching
       ) {
         slide.tinderTransform = "";
       }
